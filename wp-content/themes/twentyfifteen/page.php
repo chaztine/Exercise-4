1d51ab2c1724f6a971d1d7f524c16cb5509429db
chazzy <thechazzyness@gmail.com>
2017-08-15 07:22:16 +0800

    added changes

--- wp-content/themes/twentyfifteen/page.php
+++ wp-content/themes/twentyfifteen/page.php
@@ -1,38 +1,41 @@
 <?php
 /**
- * The template for displaying pages
+ * The template for displaying all pages
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
  */
 
 get_header(); ?>
 
+<div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+			<?php
+			while ( have_posts() ) : the_post();
 
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
+				get_template_part( 'template-parts/page/content', 'page' );
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-		// End the loop.
-		endwhile;
-		?>
+			endwhile; // End of the loop.
+			?>
 
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .wrap -->
 
-<?php get_footer(); ?>
+<?php get_footer();
